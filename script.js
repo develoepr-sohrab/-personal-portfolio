3207abd2dd463d9be7603477a24c80073893c13e
Sohrab <mdsohrab1431@gmail.com>
2024-08-03 18:19:42 +0600

    change work

--- script.js
+++ script.js
@@ -1,86 +1,110 @@
 // Script File
 
 // Home Section Starts
-var menuBtn = document.querySelector('.menu-btn');
-var menu = document.querySelector('.nav-links');
-var menuLinks = document.querySelectorAll('.nav-links li a');
+var menuBtn = document.querySelector(".menu-btn");
+var menu = document.querySelector(".nav-links");
+var menuLinks = document.querySelectorAll(".nav-links li a");
 
-menuBtn.addEventListener('click', activeClass);
+menuBtn.addEventListener("click", activeClass);
 
-function activeClass(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+function activeClass() {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
 }
 
-for(i = 0; i < menuLinks.length; i++){
-	menuLinks[i].addEventListener('click', menuItemClicked);
+for (i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener("click", menuItemClicked);
 }
 
-function menuItemClicked(){
-	menuBtn.classList.remove('active');
-	menu.classList.remove('active');
+function menuItemClicked() {
+  menuBtn.classList.remove("active");
+  menu.classList.remove("active");
 }
 
-var homeSection = document.querySelector('.home');
-window.addEventListener('scroll', scrollFunction);
-window.addEventListener('load', scrollFunction);
+var homeSection = document.querySelector(".home");
+window.addEventListener("scroll", scrollFunction);
+window.addEventListener("load", scrollFunction);
 
-function scrollFunction(){
-	if(window.scrollY > 60){
-		homeSection.classList.add('active');
-	}
-	else{
-		homeSection.classList.remove('active');
-	}
+function scrollFunction() {
+  if (window.scrollY > 60) {
+    homeSection.classList.add("active");
+  } else {
+    homeSection.classList.remove("active");
+  }
 }
-// Home Section Ends 
+// Home Section Ends
 
 // Portfolio Section Starts
-var $galleryContainer = $('.gallery').isotope({
-	itemSelector: '.item',
-	layoutMode: 'fitRows'
-})
+var $galleryContainer = $(".gallery").isotope({
+  itemSelector: ".item",
+  layoutMode: "fitRows",
+});
 
-$('.button-group .button').on('click', function(){
-	$('.button-group .button').removeClass('active');
-	$(this).addClass('active');
+$(".button-group .button").on("click", function () {
+  $(".button-group .button").removeClass("active");
+  $(this).addClass("active");
 
-	var value = $(this).attr('data-filter');
-	$galleryContainer.isotope({
-		filter: value
-	})
-})
+  var value = $(this).attr("data-filter");
+  $galleryContainer.isotope({
+    filter: value,
+  });
+});
 
 // magnific popup
-$('.gallery').magnificPopup({
-	delegate: '.overlay a',
-	type: 'image',
-	gallery:{
-		enabled: true
-	}
-})
+$(".gallery").magnificPopup({
+  delegate: ".overlay .view-more",
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
 // Portfolio Section Ends
 
 // Testimonials Section Starts
-$('.testimonials-container').owlCarousel({
-    loop:true,
-    autoplay:true,
-    autoplayTime:6000,
-    margin:10,
-    nav:true,
-    navText:["<i class='fa-solid fa-arrow-left'></i>",
-             "<i class='fa-solid fa-arrow-right'></i>"],
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:1,
-            nav:true
-        },
-        768:{
-            items:2
-        }
+$(".testimonials-container").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTime: 6000,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa-solid fa-arrow-left'></i>",
+    "<i class='fa-solid fa-arrow-right'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 1,
+      nav: true,
+    },
+    768: {
+      items: 2,
+    },
+  },
+});
+
+
+// 'GITHUB LINK' লিঙ্কের জন্য ক্লিক ইভেন্ট হ্যান্ডলার
+document.querySelectorAll(".github-link").forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    event.preventDefault();
+    var url = this.getAttribute("data-url");
+    if (url !== "#") {
+      window.open(url, "_blank");
     }
-})
+  });
+});
+
+// 'LIVE LINK' লিঙ্কের জন্য ক্লিক ইভেন্ট হ্যান্ডলার
+document.querySelectorAll(".live-link").forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    event.preventDefault();
+    var url = this.getAttribute("data-url");
+    if (url !== "#") {
+      window.open(url, "_blank");
+    }
+  });
+});
